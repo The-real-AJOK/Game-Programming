5a9f37ff9ce810d26b5947e5054c2a2f3b2d14a0
inyomanbayu <68264756+inyomanbayu@users.noreply.github.com>
2022-12-02 11:36:43 +0700

    adding countdown component and factory file

--- Assets/Countdown/Components/CountdownComponent.cs
+++ Assets/Countdown/Components/CountdownComponent.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CountdownComponent : MonoBehaviour
 {
+    private TextMeshProUGUI countdownText;
+    private float currentTime = 0f;
+    private float startingTime = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.countdownText = this.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        this.countdownText.text = "5";
+        currentTime = startingTime;
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            enabled = false;
+        }
     }
 }
