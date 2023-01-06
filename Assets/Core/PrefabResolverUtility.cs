e7446b53692ca4d1da139cc6c020a0e11cfee0a3
michaela.hannah <62824929+Michaela-alt@users.noreply.github.com>
2023-01-06 14:04:02 +0700

    remove unnecessary files

--- Assets/Core/PrefabResolverUtility.cs
+++ Assets/Core/PrefabResolverUtility.cs
@@ -1,43 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class PrefabResolverUtility : MonoBehaviour
+// public class PrefabResolverUtility : MonoBehaviour
 
-{
-    [SerializeField]
-    private GameObject enemyPrefab;
+// {
+//     [SerializeField]
+//     private GameObject enemyPrefab;
 
-    [SerializeField]
-    private GameObject playerPrefab;
+//     [SerializeField]
+//     private GameObject playerPrefab;
 
-    private static GameObject enemyPrefabStatic;
-    private static GameObject playerPrefabStatic;
+//     private static GameObject enemyPrefabStatic;
+//     private static GameObject playerPrefabStatic;
 
-    void Awake()
-    {
-        PrefabResolverUtility.enemyPrefabStatic = enemyPrefab;
-        PrefabResolverUtility.playerPrefabStatic = playerPrefab;
-    }
+//     void Awake()
+//     {
+//         PrefabResolverUtility.enemyPrefabStatic = enemyPrefab;
+//         PrefabResolverUtility.playerPrefabStatic = playerPrefab;
+//     }
 
-    public static GameObject EnemyPrefab
-    {
-        get
-        {
-            return PrefabResolverUtility.enemyPrefabStatic;
-        }
-    }
+//     public static GameObject EnemyPrefab
+//     {
+//         get
+//         {
+//             return PrefabResolverUtility.enemyPrefabStatic;
+//         }
+//     }
 
-    public static GameObject PlayerPrefab
-    {
-        get
-        {
-            return PrefabResolverUtility.playerPrefabStatic;
-        }
-    }
+//     public static GameObject PlayerPrefab
+//     {
+//         get
+//         {
+//             return PrefabResolverUtility.playerPrefabStatic;
+//         }
+//     }
 
-    void Start()
-    {
-        Debug.Log(PrefabResolverUtility.EnemyPrefab);
-    }
-}
+//     void Start()
+//     {
+//         Debug.Log(PrefabResolverUtility.EnemyPrefab);
+//     }
+// }
